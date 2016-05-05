59ec2fc777fecbffed68f4cc727746116800d99b
Phippsy <donal.phipps@gmail.com>
2016-05-05 11:25:49 +0100

    Delete twitter-auth.R

--- twitter-auth.R
+++
@@ -1,6 +0,0 @@
-api_key<- "0nPQSRwiOqYSkZmkye4duJmDj"
-api_secret<- "3G9yhWw2m35ijgyor9wNXjVUK2832OcY1OHKS9foKhGmnbDCIb"
-access_token <- "26742002-VsGJNvfAWDtfKpbuE7pQeRmHpzRtHSYSIs0FWaBqX"
-
-access_token_secret <- "SsdoyDXBrZAVLVuXGDC9VGmPuLCkLupRZBIlvocdVe34C"
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
